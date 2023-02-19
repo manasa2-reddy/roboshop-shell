37dc6db2339135eb09ea692e2bcc0f3d0aa74b50
manasa <manasa.busireddy@gmail.com>
2023-02-18 20:02:31 -0500

    print functions

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,15 @@
+source common.sh
+
+print_head :"setup MongoDB repository"
 cp config mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install MongoDB"
 yum install mongodb-org -y
+
+print_head "Enable MongoDB"
 systemctl enable mongod
+
+print_head "start MongoDB Service"
 systemctl start mongod
 
 
