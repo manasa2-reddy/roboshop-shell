60c7e92e1c5e3bbc9cf1e2b3168eb4f7f4f5106f
manasa <manasa.busireddy@gmail.com>
2023-02-18 20:52:34 -0500

    print functions

--- catalogue.sh
+++ catalogue.sh
@@ -1,23 +1,48 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+print_head "configure NodeJS Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+print_head "Install NodeJS "
+yum install nodejs -y &>>${log_file}
+
+print_head "Create Roboshop User"
+useradd roboshop &>>${log_file}
+
+print_head "Create Application Directory"
+mkdir /app &>>${log_file}
+
+print_head "Delete Old Content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading App Content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp config/catalogue.service /etc/systemd/system/catalogue.service
 
-systemctl daemon-reload
-systemctl enable mongod
-systemctl start mongod
+print_head "Extracting App Content"
+unzip /tmp/catalogue.zip &>>${log_file}
 
-cp config/mondod.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+print_head "Installing NodeJS Dependencies"
+npm install &>>${log_file}
+
+print_head "Copy SystemD Service File"
+cp config/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable Catalogue Service"
+systemctl enable mongod &>>${log_file}
+
+print_head "Start Catalogue Service"
+systemctl restart mongod &>>${log_file}
+
+print_head "Copy MongoDB Repo File"
+cp config/mondod.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "Install Mongo Client"
+yum install mongodb-org-shell -y &>>${log_file}
 
 
-mongo --host mongodb.71.store </app/schema/catalogue.js
+print_head "Load Schema"
+mongo --host mongodb.71.store </app/schema/catalogue.js &>>${log_file}
