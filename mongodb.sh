e0145ed2a0ad0ab4b2fef39cab5225eb63a7a3bc
manasa <manasa.busireddy@gmail.com>
2023-02-18 21:53:23 -0500

    print functions

--- mongodb.sh
+++ mongodb.sh
@@ -6,13 +6,15 @@ cp config/mongodb.repo /etc/yum.repos.d/mongo.repo
 print_head "Install MongoDB"
 yum install mongodb-org -y
 
+print_head"Udate MongoDB listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 print_head "Enable MongoDB"
 systemctl enable mongod
 
 print_head "start MongoDB Service"
-systemctl start mongod
+systemctl restart mongod
 
 
-#update /etc/mongod.conf file from 127.0.0.1 with 0.
+
 
 
