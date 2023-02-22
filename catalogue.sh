9f7f1bf559ff032ae9900fa265f360c0658238a5
manasa <manasa.busireddy@gmail.com>
2023-02-22 14:51:27 -0500

    print functions

--- catalogue.sh
+++ catalogue.sh
@@ -1,48 +1,6 @@
+
 source common.sh
 
-print_head "configure NodeJS Repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-
-print_head "Install NodeJS"
-yum install nodejs -y &>>${log_file}
-
-print_head "Create Roboshop User"
-useradd roboshop &>>${log_file}
-
-print_head "Create Application Directory"
-mkdir /app &>>${log_file}
-
-print_head "Delete Old Content"
-rm -rf /app/* &>>${log_file}
-
-print_head "Downloading App Content"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
-cd /app
-
-print_head "Extracting App Content"
-unzip /tmp/catalogue.zip &>>${log_file}
-
-print_head "Installing NodeJS Dependencies"
-npm install &>>${log_file}
-
-print_head "Copy SystemD Service File"
-cp ${code_dir}/config/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
-
-print_head "Reload SystemD"
-systemctl daemon-reload &>>${log_file}
-
-print_head "Enable Catalogue Service"
-systemctl enable mongod &>>${log_file}
-
-print_head "Start Catalogue Service"
-systemctl restart mongod &>>${log_file}
-
-print_head "Copy MongoDB Repo File"
-cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
-
-print_head "Install Mongo Client"
-yum install mongodb-org-shell -y &>>${log_file}
-
-
-print_head "Load Schema"
-mongo --host mongodb.devopsb71.store </app/schema/catalogue.js &>>${log_file}
+component=catalogue
+schema_type="mongo"
+nodejs
--- mongodb.sh
+++ mongodb.sh
@@ -1,21 +1,22 @@
+
 source common.sh
 
-print_head "setup MongoDB repository"
-cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+print_head "Setup MongoDB repository"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
 
 print_head "Install MongoDB"
 yum install mongodb-org -y &>>${log_file}
+status_check $?
 
-print_head "Update MongoDB listen Address"
+print_head "Update MongoDB Listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
+status_check $?
 
 print_head "Enable MongoDB"
 systemctl enable mongod &>>${log_file}
+status_check $?
 
-print_head "start MongoDB Service"
+print_head "Start MongoDB Service"
 systemctl restart mongod &>>${log_file}
-
-
-
-
-
+status_check $?
